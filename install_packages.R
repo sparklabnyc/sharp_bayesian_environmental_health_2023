1ee975952c527b12b03275e546037b6047395eb7
emmagorin <emg2237@cumc.columbia.edu>
2023-08-09 00:14:44 +0000

    add code to install packages

---
+++ install_packages.R
@@ -0,0 +1,23 @@
+# Welcome to SHARP Bayesian Modeling for Environmental Health Workshop!
+
+# All the packages you will need should be already installed when you open the project in 
+# posit cloud. 
+
+# In case you'd like to run the labs on R locally in the future, 
+# the code below installs all necessary packages, including specific code for INLA
+
+
+#install packages:
+list.of.packages <- c("here", "tidyverse", "bayesplot", "posterior", "hrbrthemes", 
+                   "colorspace", "readr", "ggplot2", "rgeos", "patchwork",
+                   "coda", "ggmcmc", "lubridate", "fastDummies", "nimble")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+#install INLA
+if(('INLA' %in% installed.packages())==F) {
+  install.packages('INLA', repos=c(getOption('repos'),INLA='https://inla.r-inla-download.org/R/testing'),dep=TRUE)
+}  
+
+
+
