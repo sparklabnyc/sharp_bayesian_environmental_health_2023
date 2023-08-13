64f81fe3005aa4f2cbac638faff377d68bb121b4
rmp15 <r.parks15@imperial.ac.uk>
2023-08-13 16:37:46 -0400

    added specific dates to labs

--- install_packages.R
+++ install_packages.R
@@ -6,17 +6,25 @@
 # In case you'd like to run the labs on R locally in the future,
 # the code below installs all necessary packages, including specific code for INLA
 
+install.packages("renv")
 
-# install packages:
-list.of.packages <- c(
-  "here", "tidyverse", "bayesplot", "posterior", "hrbrthemes",
-  "colorspace", "readr", "ggplot2", "rgeos", "patchwork",
-  "coda", "ggmcmc", "lubridate", "fastDummies", "nimble"
-)
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
-if (length(new.packages)) install.packages(new.packages)
+renv::restore()
+
+# INLA is weird so do the following
 
 # install INLA
 if (("INLA" %in% installed.packages()) == F) {
   install.packages("INLA", repos = c(getOption("repos"), INLA = "https://inla.r-inla-download.org/R/testing"), dep = TRUE)
 }
+
+# Below you can do if the above doesn't work
+
+# # install packages:
+# list.of.packages <- c(
+#   "here", "tidyverse", "bayesplot", "posterior", "hrbrthemes",
+#   "colorspace", "readr", "ggplot2", "rgeos", "patchwork",
+#   "coda", "ggmcmc", "lubridate", "fastDummies", "nimble"
+# )
+# new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
+# if (length(new.packages)) install.packages(new.packages)
+#
